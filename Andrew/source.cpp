0a6a9a3473039b27ce981eaaf8a355c5dc195939
Andrew De La Fuente <delafuenteandrew@ymail.com>
2018-04-26 15:16:45 -0700

    Coded Floyd's Alg

--- Andrew/source.cpp
+++ Andrew/source.cpp
@@ -26,6 +26,28 @@ private:
   Pitcher pitcherGroup[3];
 };
 
+int min(int a, int b)
+{
+  return (a > b)? a: b;
+}
+
+void FloydAlg(int matrix[][9])
+{
+  for(int k = 0; k < 9; k++)
+  {
+    for(int i = 0; i < 9; i++)
+    {
+      for(int j = 0; j < 9; j++)
+      {
+        // do not update shortest path to itself
+        if(i != j){
+          matrix[i][j] = min(matrix[i][j], matrix[i][k]+matrix[k][j]);
+        }
+      }
+    }
+  }
+}
+
 
 // MAIN PROGRAM
 int main(){
@@ -33,8 +55,9 @@ int main(){
   // dummy code
   // PitcherGroup a(8,8,0);
   // a.printPitcherGroup();
-  
 
+  // Demo matrix
+  
 
 
 
