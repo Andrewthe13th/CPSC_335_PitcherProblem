03ae51eedce7ec3cb3208b6c50f2560c8979d350
Unknown <PaulSmith.Contactinfo@gmail.com>
2018-05-04 11:08:36 -0700

    still seg faulting

--- Andrew/source.cpp
+++ Andrew/source.cpp
@@ -89,7 +89,7 @@ struct Node {
 Node *newnode(int a, int b, int c) {
   Node *node = new Node;
   node->data = PitcherGroup(a, b, c);
-  node->c1 = node->c2 = node->c3 = node->c4 = node->c5 = node->c6 = NULL;
+  Node *c1, *c2, *c3, *c4, *c5, *c6 = 0;
   return (node);
 }
 
@@ -102,54 +102,67 @@ public:
     v.push_back(root->data);
   }
   Node *createTree(Node *n) {
+    cout << "pointer :";
+    cout << n;
+    cout << " is found here" << endl;
+    ;
+
     // If last round didn't add anything new than quit
-    if (n == NULL)
+    if (n == NULL) {
       return NULL;
-    else {
+    } else {
       bool newPitcherCombinations = false;
       int new7pitcher = 0;
       int new9pitcher = 0;
       int new16pitcher = 0;
       // Check if the 16 pitcher is not empty
       if (n->data.pitcherGroup[0].getContents() != 0) {
+
         // check if the 9 pitcher has rooom
         if (n->data.pitcherGroup[1].hasRoom()) {
 
-          // how much the 16 pitcher will have after the move
-          new16pitcher = n->data.pitcherGroup[0].getContents() -
-                         n->data.pitcherGroup[1].space();
           // If 16pitcher has more water than the 9 pitcher has space
           if (n->data.pitcherGroup[1].space() <
               n->data.pitcherGroup[0].getContents()) {
+            // how much the 16 pitcher will have after the move
+            new16pitcher = n->data.pitcherGroup[0].getContents() -
+                           n->data.pitcherGroup[1].space();
             // fill up 9
             new9pitcher = 9;
           } else {
             // pitcher 9 is partially filled
             new9pitcher = n->data.pitcherGroup[1].getContents() +
-                          n->data.pitcherGroup[0].getContents();
+                          (n->data.pitcherGroup[0].getContents());
+            new16pitcher = 0;
           }
           // create new node from edge 16->9
+          cout << "c1 start:" << n->c1 << endl;
           n->c1 = newnode(new16pitcher, new9pitcher,
                           n->data.pitcherGroup[2].getContents());
+          cout << "c1 finish:" << n->c1 << endl;
         }
         // check if ptcher 7 has room
         if (n->data.pitcherGroup[2].hasRoom()) {
-          // how much the 16 pitcher will have after the move
-          new16pitcher = n->data.pitcherGroup[0].getContents() -
-                         n->data.pitcherGroup[2].space();
+
           // If 16pitcher has more water than the 7 pitcher has space
           if (n->data.pitcherGroup[2].space() <
               n->data.pitcherGroup[0].getContents()) {
+            // how much the 16 pitcher will have after the move
+            new16pitcher = n->data.pitcherGroup[0].getContents() -
+                           n->data.pitcherGroup[2].space();
             // fill up 7
             new7pitcher = 7;
           } else {
             // pitcher 7 is partially filled
             new7pitcher = n->data.pitcherGroup[2].getContents() +
                           n->data.pitcherGroup[0].getContents();
+            new16pitcher = 0;
           }
           // create new node from edge 16->7
-          n->c2 = newnode(new16pitcher, n->data.pitcherGroup[2].getContents(),
+          cout << "c2 start:" << n->c2 << endl;
+          n->c2 = newnode(new16pitcher, n->data.pitcherGroup[1].getContents(),
                           new7pitcher);
+          cout << "c2 finish:" << n->c2 << endl;
         }
       }
 
@@ -157,41 +170,53 @@ public:
       if (n->data.pitcherGroup[1].getContents() != 0) {
         // check if the 16 pitcher has rooom
         if (n->data.pitcherGroup[0].hasRoom()) {
-          // how much the 9 pitcher will have after the move
-          new9pitcher = n->data.pitcherGroup[1].getContents() -
-                        n->data.pitcherGroup[0].space();
+
           // If 9 pitcher has more water than the 16 pitcher has space
           if (n->data.pitcherGroup[0].space() <
               n->data.pitcherGroup[1].getContents()) {
+            // how much the 9 pitcher will have after the move
+            new9pitcher = n->data.pitcherGroup[1].getContents() -
+                          n->data.pitcherGroup[0].space();
             // fill up 16
             new16pitcher = 16;
           } else {
             // pitcher 16 is partially filled
             new16pitcher = n->data.pitcherGroup[1].getContents() +
                            n->data.pitcherGroup[0].getContents();
+            new9pitcher = 0;
           }
           // create new node from edge 9->16
+          cout << "c3 start:" << n->c3 << endl;
+
           n->c3 = newnode(new16pitcher, new9pitcher,
                           n->data.pitcherGroup[2].getContents());
         }
+        cout << "c3 finish:" << n->c3 << endl;
+
         // check if ptcher 7 has room
         if (n->data.pitcherGroup[2].hasRoom()) {
-          // how much the 9 pitcher will have after the move
-          new9pitcher = n->data.pitcherGroup[1].getContents() -
-                        n->data.pitcherGroup[2].space();
+
           // If pitcher 9 has more water than the 7 pitcher has space
           if (n->data.pitcherGroup[2].space() <
               n->data.pitcherGroup[1].getContents()) {
+            // how much the 9 pitcher will have after the move
+            new9pitcher = n->data.pitcherGroup[1].getContents() -
+                          n->data.pitcherGroup[2].space();
             // fill up 7
             new7pitcher = 7;
+
           } else {
             // pitcher 7 is partially filled
             new7pitcher = n->data.pitcherGroup[2].getContents() +
                           n->data.pitcherGroup[1].getContents();
+            new9pitcher = 0;
           }
           // create new node from edge 9->7
+          cout << "c4 start:" << n->c4 << endl;
+
           n->c4 = newnode(n->data.pitcherGroup[0].getContents(), new9pitcher,
                           new7pitcher);
+          cout << "c4 finish:" << n->c4 << endl;
         }
       }
 
@@ -199,71 +224,90 @@ public:
       if (n->data.pitcherGroup[2].getContents() != 0) {
         // check if the 16 pitcher has rooom
         if (n->data.pitcherGroup[0].hasRoom()) {
-          // how much the 7 pitcher will have after the move
-          new7pitcher = n->data.pitcherGroup[2].getContents() -
-                        n->data.pitcherGroup[0].space();
+
           // If 7 pitcher has more water than the 16 pitcher has space
           if (n->data.pitcherGroup[0].space() <
               n->data.pitcherGroup[2].getContents()) {
+            // how much the 7 pitcher will have after the mov
+            new7pitcher = n->data.pitcherGroup[2].getContents() -
+                          n->data.pitcherGroup[0].space();
             // fill up 16
             new16pitcher = 16;
+
           } else {
             // pitcher 16 is partially filled
             new16pitcher = n->data.pitcherGroup[2].getContents() +
                            n->data.pitcherGroup[0].getContents();
+            new7pitcher = 0;
           }
           // create new node from edge 7->16
+          cout << "c5 start:" << n->c5 << endl;
+
           n->c5 = newnode(new16pitcher, n->data.pitcherGroup[1].getContents(),
                           new7pitcher);
+          cout << "c5 finish:" << n->c5 << endl;
         }
         // check if ptcher 9 has room
         if (n->data.pitcherGroup[2].hasRoom()) {
-          // how much the 7 pitcher will have after the move
-          new7pitcher = n->data.pitcherGroup[2].getContents() -
-                        n->data.pitcherGroup[1].space();
+
           // If pitcher 7 has more water than the 9 pitcher has space
           if (n->data.pitcherGroup[1].space() <
               n->data.pitcherGroup[2].getContents()) {
+            // how much the 7 pitcher will have after the move
+            new7pitcher = n->data.pitcherGroup[2].getContents() -
+                          n->data.pitcherGroup[1].space();
             // fill up 9
             new9pitcher = 9;
           } else {
             // pitcher 7 is partially filled
             new7pitcher = n->data.pitcherGroup[2].getContents() +
                           n->data.pitcherGroup[1].getContents();
+            new7pitcher = 0;
           }
           // create new node from edge 7->9
+          cout << "c6 start:" << n->c6 << endl;
+
           n->c6 = newnode(n->data.pitcherGroup[0].getContents(), new9pitcher,
                           new7pitcher);
+          cout << "c6 finish:" << n->c6 << endl;
         }
       }
+
+      cout << "c1 is:" << n->c1 << endl;
+      cout << "c2 is:" << n->c2 << endl;
+      cout << "c3 is:" << n->c3 << endl;
+      cout << "c4 is:" << n->c4 << endl;
+      cout << "c5 is:" << n->c5 << endl;
+      cout << "c6 is:" << n->c6 << endl;
       // bool to check if there were any new pitcher combinations
       // checks all new pitcher combos just made
+      cout << "1" << endl;
       if (n->c1 != NULL) {
         if (std::find(v.begin(), v.end(), n->c1->data) != v.end()) {
-
         } else { // If a new pitcher combo was found throw it in the vector
           v.push_back(n->c1->data);
           newPitcherCombinations = true;
         }
       }
+      cout << "2" << endl;
       if (n->c2 != NULL) {
         if (std::find(v.begin(), v.end(), n->c2->data) != v.end()) {
-
         } else {
           // If a new pitcher combo was found throw it in the vector
           v.push_back(n->c2->data);
           newPitcherCombinations = true;
         }
       }
+      cout << "3" << endl;
       if (n->c3 != NULL) {
         if (std::find(v.begin(), v.end(), n->c3->data) != v.end()) {
-
         } else {
           // If a new pitcher combo was found throw it in the vector
           v.push_back(n->c3->data);
           newPitcherCombinations = true;
         }
       }
+      cout << "4" << endl;
       if (n->c4 != NULL) {
         if (std::find(v.begin(), v.end(), n->c4->data) != v.end()) {
 
@@ -273,6 +317,7 @@ public:
           newPitcherCombinations = true;
         }
       }
+      cout << "5" << endl;
       if (n->c5 != NULL) {
         if (std::find(v.begin(), v.end(), n->c5->data) != v.end()) {
 
@@ -282,6 +327,7 @@ public:
           newPitcherCombinations = true;
         }
       }
+      cout << "6" << endl;
       if (n->c6 != NULL) {
         if (std::find(v.begin(), v.end(), n->c6->data) != v.end()) {
 
@@ -291,15 +337,23 @@ public:
           newPitcherCombinations = true;
         }
       }
+
       // If just one new pitcher combo was found, take tree down a level
       if (newPitcherCombinations) {
-        return createTree(n->c1);
-        return createTree(n->c2);
-        return createTree(n->c3);
-        return createTree(n->c4);
-        return createTree(n->c5);
-        return createTree(n->c6);
+        createTree(n->c1);
+        cout << "test1" << endl;
+        createTree(n->c2);
+        cout << "test2" << endl;
+        createTree(n->c3);
+        cout << "test3" << endl;
+        createTree(n->c4);
+        cout << "test4" << endl;
+        createTree(n->c5);
+        cout << "test5" << endl;
+        createTree(n->c6);
+        cout << "finished one loop ##################" << endl;
       } else {
+        cout << "There was nothing new added" << endl;
         return NULL;
       }
     }
